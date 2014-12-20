f7efb8617b7519765aecc19992d28414c2bd429a
kate <katejim@mail.ru>
2014-12-21 01:07:13 +0300

    add light power, specular param, mouse

--- task2/controls.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef CONTROLS_HPP
-#define CONTROLS_HPP
-#include <GLFW/glfw3.h>
-
-void computeMatricesFromInputs(GLFWwindow * window);
-glm::mat4 getViewMatrix();
-glm::mat4 getProjectionMatrix();
-
-#endif
